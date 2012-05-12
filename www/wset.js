e23635540c481815e75294a5c06658d802b3efc5
Yura Zhloba <yzh44yzh@gmail.com>
2012-05-12 21:56:54 +0300

    remind item

--- www/wset.js
+++ www/wset.js
@@ -22,6 +22,10 @@ WorkingSet.flip = function() {
 }
 
 WorkingSet.remind = function() {
+    var currItem = this._data[this._current];
+    this._data.splice(this._current, 1);
+    this._data.push(currItem);
+    this._render();
 }
 
 WorkingSet._render = function() {
